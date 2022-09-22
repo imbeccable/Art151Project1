c6814f6f1ec978e637dee2a8aec7ef672953df9a
Becca Nika <rnika3@uic.edu>
2022-09-22 10:39:16 -0500

    changed so much stuff dudes

--- sketch.js
+++ sketch.js
@@ -1,115 +1,123 @@
-let value = 0;
-var angle = 2.0;
-var scalar = 3.5;
-var offset = 300;
-var speed = 2;
-let cnv
-recs = []
-stillrecs = []
+let blocks = [];
+let pressed = false;
+let c1 = 0;
+let c2 = 0;
+let c3 = 0;
 var col = {
     r: 255,
     g: 0,
     b: 0
 };
-//let pressed = false;
-
-function setup() {
-    createCanvas(windowWidth,windowHeight)
-    background(0)
-}
-
-function draw() {
-    
-    
-    if (mousePressed()) {
-        rec = new Rectangle(mouseX,mouseY,15,15)
-        rec.display()
-        rec.mouseClicked()
-    }
-    // for(i=0;i<stillrecs.length;i++) {
-    //     stillrecs[i].display();
-    // }
-
-    // for (i=0;i<recs.length;i++) {
-        
-    //     rec.spiral()
-    //     rec.display()
-    // }
-    
-    // if (mouseIsPressed) {
-    //     rec = new Rectangle(mouseX,mouseY,15,15)
-    //     recs.push(rec)
-    // }
-
-}
-
-class Rectangle{
-    constructor(x,y,width,height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+class Block {
+    constructor(x,y,width,height,speedx,speedy) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.speedx = speedx;
+      this.speedy = speedy;
     }
 
-    display() {    
+    display() {
         col.r = random(0, 200);
         col.g = random(0, 250);
         col.b = random(100, 250); 
-        fill(col.r, col.g, col.b);   
-        rect(this.x,this.y,this.width,this.height)
+        fill(col.r, col.g, col.b);
+        rect(this.x,this.y,this.width,this.height);
     }
 
-    mouseClicked() {
-        var x = offset + cos(angle) * scalar;
-        var y = offset + sin(angle) * scalar;
-        ellipse(x, y, 5, 5);
-        angle += speed;
-        scalar += speed;
-        return false
+    move() {
+        this.x += this.speedx;
+        if (this.x > windowWidth - this.width || this.x < 0) {
+            this.speedx *= -1;
+        }
+        this.y += this.speedy;
+        if (this.y > windowHeight - this.height || this.y < 0) {
+            this.speedy *= -1;
+        }        
+    }   
+    
+    // scaling() {
+    //     if (this.x < 5) {
+    //         scale(2)
+    //     }
+    //     if (this.x > 200) {
+    //         scale(-2)
+    //     }
+    //     if (this.y < 5) {
+    //         scale(0,2)
+    //     }
+    //     if (this.y > 200) {
+    //         scale(0,-2)
+    //     }
+    }
+  }
+
+function reset() {
+    blocks.length = 0;
+    c1 = 0;
+    c2 = 0;
+    c3 = 0;
+}
+
+function mouseClicked() {
+    if (mouseIsPressed) {
+        return true;
+    }
+    return false;
+}
+
+function changeBackground1() {
+    col.r = random(0, 200);
+    return col.r
+}
+function changeBackground2() {
+    col.g = random(0, 250);
+    return col.g
+}
+function changeBackground3() {
+    col.b = random(100, 250);
+    return col.b
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);    
+  }
+  
+function draw() { 
+    background(c1,c2,c3)
+
+    if (mouseClicked()) {
+        if (mouseX > windowWidth/2 && mouseY < windowHeight/2){
+            blocks.push(new Block(mouseX, mouseY, random(10,200), random(10,200), random(-5,-20), random(5,20))) 
+        }
+        else if (mouseX > windowWidth/2 && mouseY > windowHeight/2){
+            blocks.push(new Block(mouseX, mouseY, random(10,200), random(10,200), random(-5,-20), random(-5,-20))) 
+        }
+        else if (mouseX < windowWidth/2 && mouseY > windowHeight/2){
+            blocks.push(new Block(mouseX, mouseY, random(10,200), random(10,200), random(5,20), random(-5,-20))) 
+        }
+        else {
+            blocks.push(new Block(mouseX, mouseY, random(10,200), random(10,200), random(5,20), random(5,20)))
+        }
+
+    }
+    let i = 0
+    for (i;i<blocks.length;i++) {
+        blocks[i].display();
+        blocks[i].move();
+        //blocks[i].scaling();
+    }
+    
+    if (keyCode == 27){
+        reset();
+        keyCode = 0;
     }
 
-    // spiral() {        
-    //     this.x += cos(angle) * scalar;
-    //     this.y += sin(angle) * scalar;
-    //     noStroke();
-    //     angle += speed;
-    //     scalar += speed;
-    //   }   
-    
-}
-
-// function mouseClicked() {
-//     var x = offset + cos(angle) * scalar;
-//     var y = offset + sin(angle) * scalar;
-//     ellipse(x, y, 5, 5);
-//     angle += speed;
-//     scalar += speed;
-//     return false
-// }
-
-function mousePressed() {
-    
-}
-
-// function mouseReleased() {
-//     for (i=0;i<recs.length;i++) {
-//         stillrecs.push(recs[i]);
-//     }
-//     recs = []
-// }
-
-// keyValue = 0
-// function keyPressed() {
-//     if (keyCode === 65) {
-//         console.log('keyPressed is A')
-//         keyValue = 100
-//     }
-//     if (keyCode ===83) {
-//         console.log('keyPressed is S')
-//         keyValue = 50
-//     }
-//     else {
-//         console.log('key released')
-//     }
-// }
-
+    if (keyCode == 13) {
+        c1 = changeBackground1();
+        c2 = changeBackground2();
+        c3 = changeBackground3();
+        keyCode = 0;
+    }
+  }
